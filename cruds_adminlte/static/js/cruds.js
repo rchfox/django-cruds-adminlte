a9067904dfa1cfee0a4c0eecaf82d26b975f5018
Rolando Chaparro Fox <rchfox@dataworks.com.py>
2023-08-08 18:59:49 -0400

    Upgrade AdminLTS v2.3.7 > v2.4.18 y fixes de jquery

--- cruds_adminlte/static/js/cruds.js
+++ cruds_adminlte/static/js/cruds.js
@@ -1,7 +1,7 @@
-$(function(){
+(function($){
       $('select').select2();
       $('input').iCheck({
         checkboxClass: 'icheckbox_minimal-blue',
         radioClass: 'iradio_minimal-blue',
       });
-});
+})(jQuery);
